db557690f800205b641c549fee2a013dea44d0dd
Besma96 <kaouane.besma@gmail.com>
2019-05-07 15:59:06 +0200

    display unknow person

--- TestFace.py
+++ TestFace.py
@@ -2,6 +2,10 @@ import cozmo
 from cozmo.util import degrees, distance_mm, speed_mmps
 import time
 import asyncio
+try:
+    from PIL import Image
+except:
+    print("Looks like you need to install Pillow")
 
 
 def program_cozmo(robot: cozmo.robot.Robot):
@@ -50,11 +54,22 @@ def program_cozmo(robot: cozmo.robot.Robot):
                 image_inconnu = robot.world.latest_image.raw_image
                 image_inconnu.save("inconnu" + str(numero_inconnu) + ".bmp")
                 numero_inconnu = numero_inconnu + 1
+                inconnu_face_display(robot, "inconnu" + str(numero_inconnu - 1) + ".bmp")
                 robot.set_all_backpack_lights(cozmo.lights.white_light)
-                robot.say_text("Inconnu",False,use_cozmo_voice=False,voice_pitch=-9, in_parallel=True).wait_for_completed()
+                robot.say_text("Bonjour inconnu" + str(numero_inconnu),False,use_cozmo_voice=False,voice_pitch=-9, in_parallel=True).wait_for_completed()
             
             face_to_follow = None
             time.sleep(3)
 		
+def inconnu_face_display(robot: cozmo.robot.Robot, name_inconnu: str):
+        
+    image = Image.open(name_inconnu)
+    image = image.resize(cozmo.oled_face.dimensions(), Image.NEAREST)
+    image = cozmo.oled_face.convert_image_to_screen_data(image)
+
+    seconds =3
+
+    for nothing in range(seconds):
+        robot.display_oled_face_image(image, 1000.0)
 
 cozmo.run_program(program_cozmo, use_viewer=True, use_3d_viewer=True)
